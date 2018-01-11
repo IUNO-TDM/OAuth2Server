689119feee33aeec92a0543865c5407c1e3418ae
TRUMPF\ElyGomesMa <marcel.elygomes@de.trumpf.com>
2018-01-11 10:50:30 +0100

    Updated CreateLog Function - HotFix due to Sql Injection closes #44

--- database/patches/iuno_oauthdb_V0002V_20180110.sql
+++ database/patches/iuno_oauthdb_V0002V_20180110.sql
@@ -33,7 +33,7 @@ $$
 	DECLARE
 		PatchName varchar		 	 := 'iuno_oauthdb_V0002V_20180110';
 		PatchNumber int 		 	 := 0002;
-		PatchDescription varchar 	 := 'Create GetRefreshToken function';
+		PatchDescription varchar 	 := 'The createlog function will be updated due to sql injection problem';
 		CurrentPatch int 			 := (select max(p.patchnumber) from patches p);
 
 	BEGIN	
@@ -65,35 +65,8 @@ $$
         LANGUAGE 'plpgsql'
     AS $BODY$
 
-          DECLARE vLogID integer:= (select nextval('LogID'));
-              vSqlCmd varchar :=  concat(
-                                            'INSERT INTO LogTable(LogID, LogStatusID, LogMessage, LogObjectName, Parameters,CreatedAt)',
-                                            'VALUES( ',
-                                            cast(vLogID as varchar),
-                                            ', ',
-                                            cast(vLogStatusID as varchar),
-                                            ', ''',
-                                            vLogMessage,
-                                            ''', ''',
-                                            vLogObjectName,
-                                            ''', ''',
-                                            vParameters,
-                                            ''', ''',
-                                            now(),
-                                            ''')'
-                                       );
-
-             vConnName text := 'conn';
-             vConnString text := 'dbname=oauthdb port=5432 host=localhost user=oauthdb_loguser password=PASSWORD';
-             vConnExist bool := (select ('{' || vConnName || '}')::text[] <@ (select dblink_get_connections()));
           BEGIN
 
-            if(not vConnExist or vConnExist is null) then
-                    perform dblink_connect(vConnName,vConnString);
-            end if;
-                    perform dblink(vConnName,vSqlCmd);
-                    perform dblink_disconnect(vConnName);
-                    set role postgres;
           END;
 
     $BODY$;
