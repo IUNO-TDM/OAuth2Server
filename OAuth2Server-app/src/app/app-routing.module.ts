0b5cd44f1b729a8c0b92026ede7772cab907d864
liefersfl <florian@liefers.com>
2018-01-24 13:44:57 +0100

    added resend-email-verification page.

--- OAuth2Server-app/src/app/app-routing.module.ts
+++ OAuth2Server-app/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ResendEmailVerificationComponent } from './resend-email-verification/resend-email-verification.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'resend-email-verification/:email', component: ResendEmailVerificationComponent},
   {path: '**', redirectTo: 'login'},
 ];
 
